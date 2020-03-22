975b4bccc46560c5d8b6c4d14414ebd687d8463e
arianaandersoncodebank <61384482+arianaandersoncodebank@users.noreply.github.com>
2020-03-22 09:52:52 +0530

    Create bx-slider.php

---
+++ bx-slider.php
@@ -0,0 +1,6 @@
+bx slider
+
+https://bxslider.com/examples/
+https://bxslider.com/examples/image-slideshow-captions/
+https://bxslider.com/examples/auto-show-start-stop-controls/
+https://bxslider.com/options/
